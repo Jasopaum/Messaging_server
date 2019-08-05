0fd805a40bca828e0e9500644db1d1cb571a5acb
paumier_jas <jason.paumier@supelec.fr>
2019-08-06 00:15:54 +0200

    added some docstring

--- server.py
+++ server.py
@@ -6,6 +6,11 @@ CLIENTS = {}
 
 
 async def register(name, websocket):
+    """ When a new client arrives:
+    - check that its name is not already used
+    - notify all the users that he arrived
+    - send him all the name of all the users present
+    """
     if name in CLIENTS:
         to_send = json.dumps({"cmd": "usedname", "usr": f"{name}"})
         await websocket.send(to_send)
@@ -15,32 +20,40 @@ async def register(name, websocket):
         CLIENTS[name] = websocket
 
 async def unregister(websocket):
-    # Remove client and corresponding websocket from dict of clients
-    for k,v in CLIENTS.items():
-        if v == websocket:
-            v.close()
-            del CLIENTS[k]
-            break
-    await notify_rm_user(k)  # k is the client who left
+    """Remove client and corresponding websocket from dict of clients
+    """
+    if CLIENTS:
+        for k,v in CLIENTS.items():
+            if v == websocket:
+                v.close()
+                del CLIENTS[k]
+                break
+        await notify_rm_user(k)  # k is the client who left
 
 async def notify_new_user(name):
-    # When a new client joins, add it to everybody's lists of users
+    """When a new client joins, add it to everybody's lists of users
+    """
     to_send = json.dumps({"cmd": "addusr", "usr": [name]})
     if CLIENTS:
         await asyncio.wait([ws.send(to_send) for c, ws in CLIENTS.items()])
 
 async def init_list_users(ws):
-    # When a new client joins, init its list of users
+    """When a new client joins, init its list of users
+    """
     to_send = json.dumps({"cmd": "addusr", "usr": list(CLIENTS.keys())})
     if CLIENTS:
         await ws.send(to_send)
 
 async def notify_rm_user(name):
+    """When a new client leaves, remove it from everybody's lists of users
+    """
     to_send = json.dumps({"cmd": "rmusr", "usr": name})
     if CLIENTS:
         await asyncio.wait([c.send(to_send) for c in CLIENTS.values()])
 
 async def send_msg(src, dst, msg):
+    """ Send a message to a client via a websocket
+    """
     try:
         to_send = json.dumps({"cmd": "recv", "src": src, "msg": msg})
         await CLIENTS[dst].send(to_send)
@@ -49,6 +62,10 @@ async def send_msg(src, dst, msg):
         await CLIENTS[src].send(to_send)
 
 async def main(websocket, path):
+    """ This is the main function (the one continuously executed)
+    When a message is received from a websocket, it is read and
+    the cmd field tells what type of message was sent and what to do.
+    """
     try:
         async for message in websocket:
             message = json.loads(message)
